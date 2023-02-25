8e42e74e5198b43a57e5c3507e9fd8746354f7e9
klm hyeon woo <gentlemonster77@naver.com>
2023-02-25 17:19:55 +0900

    파이썬 소스코드 수정
    파이썬 소스코드 수정

--- auto/parser.py
+++ auto/parser.py
@@ -6,22 +6,21 @@ feed = feedparser.parse(tistory_blog_uri+"/rss")
 
 markdown_text = """
 
-![dino.gif](./dino.gif)
-
 <div align="center">
-<img style="height:110px" src="https://user-images.githubusercontent.com/19422885/206861312-7dbd3708-98dc-4b97-82e9-96f25581bc94.gif"></img>
+<img style="height:70px" src="https://user-images.githubusercontent.com/19422885/206861312-7dbd3708-98dc-4b97-82e9-96f25581bc94.gif"></img>
+
+  
+![dino.gif](./dino.gif)
 </div>
 
-## 끊임없이 브랜딩하는 FE 개발자 👋🏻
+## 끊임없이 브랜딩하는 개발자  👋🏻
 
-<span style="color:#4E5968;">
-**사용자의 편안한 UX와 UI를 중요시 하는 프론트엔드 개발자** **김현우**입니다.<br/>
+<span style="color:#4E5968; font-size:10px;">
+<strong>사용자의 편안한 UX와 UI를 중요시 하는 프론트엔드 개발자</strong>입니다.<br/>
 소프트웨어 공학을 전공하고, 다채로운 사람이 되고자<br/>
 개발 및 디자인과 기획 분야, 그리고 사람들이 소통하는 방법을 공부합니다.<br/>
 사람들에게 편리함과 도움을 제공하는 매체에 관심이 많습니다.<br/>
 때문에 다양한 부분을 도전하고, 탐구하며 다양한 프로젝트를 개발해왔습니다.</span>
-
-<img src="https://komarev.com/ghpvc/?username=evan-moon&label=Profile%20views&color=0e75b6&style=flat" alt="evan-moon" />
 
 ### 최근 포스팅
 """

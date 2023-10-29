d867ba859575a6776b8a366282d84c15a54443cd
klm hyeon woo <gentlemonster77@naver.com>
2023-10-29 11:02:04 +0900

    Update parser.py

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ import datetime
 import os
 import json
 
-tistory_blog_uri = "https://klmhyeonwooo.tistory.com/"
+feed_list = ["https://klmhyeonwooo.tistory.com/", "https://minwoo-it-factory.tistory.com"]
 feed = feedparser.parse(tistory_blog_uri+"/rss")
 
 markdown_text = """
@@ -38,17 +38,18 @@ BASE_DIR += "/data"
 print(BASE_DIR)
 uniqueKey = 0
 
-for i in feed['entries']:
-    # dt = datetime.datetime.strptime(
-    #     i['published'], "%a, %d %b %Y %H:%M:%S %z").strftime("%b %d, %Y")
-    markdown_text += f"- [{i['title']}]({i['link']})<br>\n"
-    parsing_data[uniqueKey] = {
-      "title" : i['title'],
-      "link" : i['link'],
-      "date" : datetime.datetime.strptime(i['published'], "%a, %d %b %Y %H:%M:%S %z").strftime("%b %d, %Y")
-    }
-    print("-", i['link'], i['title'])
-    uniqueKey += 1
+for tistory_blog_uri in feed_list:
+    feed = feedparser.parse(tistory_blog_uri+"/rss")
+    for i in feed['entries']:
+        # print(i)
+        markdown_text += f"- [{i['title']}]({i['link']})<br>\n"
+        parsing_data[uniqueKey] = {
+            "title" : i['title'],
+            "link" : i['link'],
+            "date" : datetime.datetime.strptime(i['published'], "%a, %d %b %Y %H:%M:%S %z").strftime("%b %d, %Y")
+        }
+        print("-", i['link'], i['title'])
+        uniqueKey += 1
 
 parsing_data = dict(sorted(parsing_data.items(), key=lambda item: datetime.datetime.strptime(item[1]['date'], '%b %d, %Y'), reverse=True))
   

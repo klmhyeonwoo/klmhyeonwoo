b4f2ff80a21f03695a82a72b1cc7e12bd53c7895
klm hyeon woo <gentlemonster77@naver.com>
2024-05-13 00:43:48 +0900

    Update parser.py

--- parser.py
+++ parser.py
@@ -18,10 +18,10 @@ markdown_text = """
 브랜딩을 좋아하는 개발자입니다 🦄
 
 ### 회고록
-- [정든 멋쟁이사자처럼을 떠나며](https://klmhyeonwooo.tistory.com/89)<br>
-- [경험의 미학](https://klmhyeonwooo.tistory.com/80)<br>
-- [리크루팅을 위해 어플라이 사이트를 개발했다](https://klmhyeonwooo.tistory.com/74)<br>
+- [개발자는 내가 만드는 제품에 대한 애정을 가져야한다](https://klmhyeonwooo.tistory.com/122)<br>
 - [그 동안 남의 시선에서 벗어나지 못했던 것 같다](https://klmhyeonwooo.tistory.com/65)<br>
+- [정든 멋쟁이사자처럼을 떠나며](https://klmhyeonwooo.tistory.com/89)<br>
+- [리크루팅을 위해 어플라이 사이트를 개발했다](https://klmhyeonwooo.tistory.com/74)<br>
 
 ### 최근 포스팅
 """

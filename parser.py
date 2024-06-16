8822c3ff57ebf201fbac0f6a25f39aeed03b1ea2
klm hyeon woo <gentlemonster77@naver.com>
2024-06-16 15:12:24 +0900

    Update parser.py

--- parser.py
+++ parser.py
@@ -17,7 +17,6 @@ feed_list = ["https://klmhyeonwooo.tistory.com?프론트엔드 김현우",
 markdown_text = """
 <div align="center">
   
-# ![dino.gif](./dino.gif)
 <img src="https://render.gitanimals.org/farms/{klmhyeonwoo}" height="220"/>
 
 </div>
@@ -25,7 +24,7 @@ markdown_text = """
 ## klm hyeon woo
 
 <span style="color:#4E5968; font-size:10px;">
-네트워킹이랑 브랜딩할 때 제일 행복한 개발자입니다 🏄🏼‍♂️
+네트워킹이랑 브랜딩할 때 제일 햄볶음밥 개발자입니다 🏄🏼‍♂️
 
 ### 회고록
 - [개발자는 내가 만드는 제품에 대한 애정을 가져야한다](https://klmhyeonwooo.tistory.com/122)<br>

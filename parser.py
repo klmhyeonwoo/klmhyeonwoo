68340b72ae4d0e5ab75ec2774163e0db357ec77d
dori <gentlemonster77@naver.com>
2025-06-22 23:08:56 +0900

    Update parser.py

--- parser.py
+++ parser.py
@@ -20,23 +20,23 @@ feed_list = ["https://klmhyeonwooo.tistory.com?느릿느릿 프론트엔드",
             ]
 
 markdown_text = """
-<div align="center">
+<div align="left">
   
-<img src="https://render.gitanimals.org/lines/klmhyeonwoo?pet-id=590059497944971134" width="1000" height="240"/>
+![마인크래프트](https://github.com/user-attachments/assets/fb05570c-cfe5-4fd7-9d86-56ff2cec345f)
 
 </div>
 
-## klm hyeon woo
+## Hola ! Hej !
 
 <span style="color:#4E5968; font-size:10px;">
 
-### 회고록
+### 회고를 주기적으로 진행해요
 - [여러 프로덕트를 마주하며 느꼈던 프로덕트에 대한 고찰](https://klmhyeonwooo.tistory.com/172)<br>
 - [프로젝트를 진행하면서 오너쉽을 가져야하는 이유들](https://klmhyeonwooo.tistory.com/149)<br>
 - [개발자는 내가 만드는 제품에 대한 애정을 가져야한다](https://klmhyeonwooo.tistory.com/122)<br>
 - [4월이 되서야 하는 프론트엔드 회고](https://klmhyeonwooo.tistory.com/167)<br>
 
-### 최근 포스팅
+### 블로그에서 다양한 내용들을 포스팅해요
 """
 
 lst = []
@@ -87,7 +87,7 @@ with open(os.path.join(BASE_DIR, 'feed.json'), 'w+',encoding='utf-8') as json_fi
 print(parsing_data)
 
 markdown_text += """
-### 경험
+### 제 경험들이 궁금하시다면
 - 이력 · http://bit.ly/3GBXpKD <br/>
 - 포트폴리오 · http://bit.ly/4cPxTO7
 """

c17085ea0795efadd66d8bdc76cff4993927d023
klmhyeonwoo <gentlemonster77@naver.com>
2025-09-24 00:17:51 +0900

    Update parser.py

--- parser.py
+++ parser.py
@@ -23,10 +23,11 @@ markdown_text = """
 <span style="color:#4E5968; font-size:10px;">
 
 #### 개발자 회고
+- [마이그레이션을 진행하며 느꼈던 팀을 위한 소소한 팁](https://klmhyeonwooo.tistory.com/183)<br>
 - [여러 프로덕트를 마주하며 느꼈던 프로덕트에 대한 고찰](https://klmhyeonwooo.tistory.com/172)<br>
 - [프로젝트를 진행하면서 오너쉽을 가져야하는 이유들](https://klmhyeonwooo.tistory.com/149)<br>
 - [개발자는 내가 만드는 제품에 대한 애정을 가져야한다](https://klmhyeonwooo.tistory.com/122)<br>
-- [4월이 되서야 하는 프론트엔드 회고](https://klmhyeonwooo.tistory.com/167)<br>
+- [2년차 프론트엔드 회고](https://klmhyeonwooo.tistory.com/167)<br>
 
 #### 테크 블로그
 """

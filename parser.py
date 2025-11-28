5336aa03948fdcf0c19806ad2ce9eb4444e25f6a
Timmy <gentlemonster77@naver.com>
2025-11-28 12:25:50 +0900

    Update parser.py

--- parser.py
+++ parser.py
@@ -20,16 +20,23 @@ feed_list = ["https://klmhyeonwooo.tistory.com?느릿느릿 프론트엔드",
             ]
 
 markdown_text = """
+
+#### 사람들에게 도움을 주는 서비스들을 만들고, 그로스하는 과정을 좋아해요
+
+[![Resume Badge](https://img.shields.io/badge/Resume-e5e5e5?logo=notion&logoColor=1a1a1a)](https://bit.ly/485bjRb)
+[![Blog Badge](https://img.shields.io/badge/Blog-f4702e?logo=bloglovin&logoColor=white)](https://klmhyeonwooo.tistory.com/)
+[![Linkdin Badge](https://img.shields.io/badge/LinkdIn-398af3?logo=logmein&logoColor=white)](www.linkedin.com/in/hyeonwoo-klm)
+
 <span style="color:#4E5968; font-size:10px;">
 
-#### 개발자 회고
+##### 개발자 회고
 - [마이그레이션을 진행하며 느꼈던 팀을 위한 소소한 팁](https://klmhyeonwooo.tistory.com/183)<br>
 - [여러 프로덕트를 마주하며 느꼈던 프로덕트에 대한 고찰](https://klmhyeonwooo.tistory.com/172)<br>
 - [프로젝트를 진행하면서 오너쉽을 가져야하는 이유들](https://klmhyeonwooo.tistory.com/149)<br>
 - [개발자는 내가 만드는 제품에 대한 애정을 가져야한다](https://klmhyeonwooo.tistory.com/122)<br>
 - [2년차 프론트엔드 회고](https://klmhyeonwooo.tistory.com/167)<br>
 
-#### 테크 블로그
+##### 테크 블로그
 """
 
 lst = []
@@ -81,7 +88,7 @@ print(parsing_data)
 
 markdown_text += """
 
-#### 패키지
+##### 패키지
 <ul style="list-style: none; padding: 0; margin: 0; font-family: sans-serif; line-height: 1.8;">
   <li style="display: flex; align-items: center; gap: 8px; margin-bottom: 6px;">
     <img width="17" height="auto" alt="icon" src="https://github.com/user-attachments/assets/69ee1f1e-b92d-45b8-b534-941ee871efd4" />
@@ -100,12 +107,26 @@ markdown_text += """
   </li>
 </ul>
 
-#### 익스텐션
+##### 익스텐션
 <ul style="list-style: none; padding: 0; margin: 0; font-family: sans-serif; line-height: 1.8;">
   <li style="display: flex; align-items: center; gap: 8px; margin-bottom: 6px;">
     <img width="17" height="auto" alt="icon" src="https://lh3.googleusercontent.com/eTtZ8knd4vZNHki7EAPmjtpFYwGETSrP3fh6iGnX4tQy4-1Zb11ArU2zphDMZq1AU29smycrkv8ReS_vDMDdvGOZ=s120" />
     <a href="https://chromewebstore.google.com/detail/rework/mibhdihagebcbcmifindjenkaoikajim?hl=ko" style="text-decoration: none; color: #0366d6; font-weight: 600;">rework</a>
     <span style="color: #6a737d; font-size: 14px;"> · A Google extension that lets you easily manage your day's tasks.</span>
+  </li>
+</ul>
+
+##### 유저들과 운영중인 서비스
+<ul style="list-style: none; padding: 0; margin: 0; font-family: sans-serif; line-height: 1.8;">
+  <li style="display: flex; align-items: center; gap: 8px; margin-bottom: 6px;">
+    <img width="17" height="auto" alt="icon" src="https://raw.githubusercontent.com/klmhyeonwoo/Asset-Archieve./2bbd60dd4a0fe38d69ee454a33adecb0538a539c/nklcb.svg" />
+    <a href="https://nklcb.io" style="text-decoration: none; color: #0366d6; font-weight: 600;">nklcb</a>
+    <span style="color: #6a737d; font-size: 14px;"> · A community service focused on big tech recruitment and employment.</span>
+  </li>
+  <li style="display: flex; align-items: center; gap: 8px; margin-bottom: 6px;">
+    <img width="17" height="auto" alt="icon" src="https://raw.githubusercontent.com/klmhyeonwoo/Asset-Archieve./2bbd60dd4a0fe38d69ee454a33adecb0538a539c/white_layer.svg" />
+    <a href="https://layerapp.io" style="text-decoration: none; color: #0366d6; font-weight: 600;">layer</a>
+    <span style="color: #6a737d; font-size: 14px;"> · A service that provides easy retrospective writing and AI analysis.</span>
   </li>
 </ul>
 """
